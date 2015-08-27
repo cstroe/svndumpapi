d67f77d8d346ecaa97193f47a011d7b8999ff250
Cosmin Stroe <cstroe@backstopsolutions.com>
2015-08-27 00:37:03 -0500

    Correctly parse svn properties.

--- src/test/java/com/github/cstroe/svndumpgui/internal/SvnDumpFileParserTest.java
+++ src/test/java/com/github/cstroe/svndumpgui/internal/SvnDumpFileParserTest.java
@@ -1,6 +1,7 @@
 package com.github.cstroe.svndumpgui.internal;
 
 import com.github.cstroe.svndumpgui.api.SvnDump;
+import com.github.cstroe.svndumpgui.api.SvnProperties;
 import com.github.cstroe.svndumpgui.api.SvnRevision;
 import com.github.cstroe.svndumpgui.generated.ParseException;
 import com.github.cstroe.svndumpgui.generated.SvnDumpFileParser;
@@ -10,11 +11,39 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 
 public class SvnDumpFileParserTest {
+
+    @Test
+    public void simple_property_is_parsed() throws ParseException {
+        final InputStream s = Thread.currentThread().getContextClassLoader()
+                .getResourceAsStream("dumps/partial/simple_property.fragment");
+
+        SvnDumpFileParser parser = new SvnDumpFileParser(s);
+
+        SvnRevisionImpl revision = new SvnRevisionImpl(0);
+        parser.Property(revision);
+
+        assertNotNull(revision.getProperty(SvnProperties.DATE));
+        assertThat(revision.getProperty(SvnProperties.DATE), is(equalTo("2015-08-07T13:52:20.465543Z")));
+    }
+
+    @Test
+    public void empty_revision_is_parsed() throws ParseException {
+        final InputStream s = Thread.currentThread().getContextClassLoader()
+                .getResourceAsStream("dumps/partial/empty_revision.fragment");
+
+        SvnDumpFileParser parser = new SvnDumpFileParser(s);
+        SvnRevision revision = parser.Revision();
+
+        assertNotNull(revision);
+        assertThat(revision.getNumber(), is(0));
+        assertThat(revision.getProperty(SvnProperties.DATE), is(equalTo("2015-08-07T13:52:20.465543Z")));
+    }
 
     @Test
     public void should_parse_empty_dump() throws IOException, ParseException {
@@ -33,8 +62,8 @@ public class SvnDumpFileParserTest {
         SvnRevision firstRevision = revisionList.get(0);
 
         assertThat(firstRevision.getNumber(), is(0));
-//        assertNotNull(firstRevision.getProperty(SvnProperties.DATE));
-//        assertThat(firstRevision.getProperty(SvnProperties.DATE), is(equalTo("2015-08-07T13:52:20.465543Z")));
+        assertNotNull(firstRevision.getProperty(SvnProperties.DATE));
+        assertThat(firstRevision.getProperty(SvnProperties.DATE), is(equalTo("2015-08-07T13:52:20.465543Z")));
     }
 
 }
