094f121cf6cc58e7ec50ea1da46292b5516deef9
Cosmin Stroe <cstroe@backstopsolutions.com>
2015-08-29 06:53:22 -0500

    Mutator to change the action of a node.

---
+++ src/test/java/com/github/cstroe/svndumpgui/internal/transform/NodeActionChangeTest.java
@@ -0,0 +1,45 @@
+package com.github.cstroe.svndumpgui.internal.transform;
+
+import com.github.cstroe.svndumpgui.api.SvnDump;
+import com.github.cstroe.svndumpgui.api.SvnDumpMutator;
+import com.github.cstroe.svndumpgui.api.SvnNode;
+import com.github.cstroe.svndumpgui.api.SvnNodeHeader;
+import com.github.cstroe.svndumpgui.generated.ParseException;
+import com.github.cstroe.svndumpgui.internal.SvnDumpFileParserTest;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
+
+public class NodeActionChangeTest {
+
+    @Test
+    public void change_action() throws ParseException {
+        SvnDump dump = SvnDumpFileParserTest.parse("dumps/svn_multi_file_delete.dump");
+
+        assertThat(dump.getRevisions().size(), is(3));
+        assertThat(dump.getRevisions().get(0).getNodes().size(), is(0));
+        assertThat(dump.getRevisions().get(1).getNodes().size(), is(3));
+        assertThat(dump.getRevisions().get(2).getNodes().size(), is(3));
+        SvnNode node = dump.getRevisions().get(2).getNodes().get(1);
+        assertThat(node.get(SvnNodeHeader.ACTION), is(equalTo("delete")));
+        assertNull(node.get(SvnNodeHeader.KIND));
+        assertThat(node.get(SvnNodeHeader.PATH), is(equalTo("README2.txt")));
+
+        SvnDumpMutator actionChange = new NodeActionChange(2, "delete", "README2.txt", "add");
+        actionChange.mutate(dump);
+
+        assertThat(dump.getRevisions().size(), is(3));
+        assertThat(dump.getRevisions().get(0).getNodes().size(), is(0));
+        assertThat(dump.getRevisions().get(1).getNodes().size(), is(3));
+        assertThat(dump.getRevisions().get(2).getNodes().size(), is(3));
+
+        SvnNode changedNode = dump.getRevisions().get(2).getNodes().get(1);
+        assertThat(changedNode.get(SvnNodeHeader.ACTION), is(equalTo("add")));
+        assertNull(changedNode.get(SvnNodeHeader.KIND));
+        assertThat(changedNode.get(SvnNodeHeader.PATH), is(equalTo("README2.txt")));
+    }
+
+}
