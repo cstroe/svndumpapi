846e1b7a54183857071adb9d631b93a8aeecb0c9
Cosmin Stroe <cstroe@backstopsolutions.com>
2015-10-02 06:28:18 -0500

    Fix ConsumerChainTest to use correct consume method.

--- src/test/java/com/github/cstroe/svndumpgui/internal/transform/ConsumerChainTest.java
+++ src/test/java/com/github/cstroe/svndumpgui/internal/transform/ConsumerChainTest.java
@@ -1,7 +1,7 @@
 package com.github.cstroe.svndumpgui.internal.transform;
 
 import com.github.cstroe.svndumpgui.api.SvnDump;
-import com.github.cstroe.svndumpgui.api.SvnDumpMutator;
+import com.github.cstroe.svndumpgui.api.SvnDumpConsumer;
 import com.github.cstroe.svndumpgui.api.SvnNode;
 import com.github.cstroe.svndumpgui.api.SvnNodeHeader;
 import com.github.cstroe.svndumpgui.generated.ParseException;
@@ -18,32 +18,31 @@ public class ConsumerChainTest {
 
     @Test
     public void mutator_chain_with_single_mutator() throws ParseException {
-        SvnDump dump = SvnDumpFileParserTest.parse("dumps/svn_multi_file_delete.dump");
+        {
+            SvnDump dump = SvnDumpFileParserTest.parse("dumps/svn_multi_file_delete.dump");
 
-        assertThat(dump.getRevisions().size(), is(3));
-        assertThat(dump.getRevisions().get(0).getNodes().size(), is(0));
-        assertThat(dump.getRevisions().get(1).getNodes().size(), is(3));
-        assertThat(dump.getRevisions().get(2).getNodes().size(), is(3));
-        SvnNode node = dump.getRevisions().get(2).getNodes().get(1);
-        assertThat(node.get(SvnNodeHeader.ACTION), is(equalTo("delete")));
-        assertNull(node.get(SvnNodeHeader.KIND));
-        assertThat(node.get(SvnNodeHeader.PATH), is(equalTo("README2.txt")));
+            assertThat(dump.getRevisions().size(), is(3));
+            assertThat(dump.getRevisions().get(0).getNodes().size(), is(0));
+            assertThat(dump.getRevisions().get(1).getNodes().size(), is(3));
+            assertThat(dump.getRevisions().get(2).getNodes().size(), is(3));
+            SvnNode node = dump.getRevisions().get(2).getNodes().get(1);
+            assertThat(node.get(SvnNodeHeader.ACTION), is(equalTo("delete")));
+            assertNull(node.get(SvnNodeHeader.KIND));
+            assertThat(node.get(SvnNodeHeader.PATH), is(equalTo("README2.txt")));
+        } {
+            SvnDumpConsumer nodeRemove = new NodeRemove(2, "delete", "README2.txt");
+            SvnDump dump = SvnDumpFileParserTest.consume("dumps/svn_multi_file_delete.dump", nodeRemove);
 
+            assertThat(dump.getRevisions().size(), is(3));
+            assertThat(dump.getRevisions().get(0).getNodes().size(), is(0));
+            assertThat(dump.getRevisions().get(1).getNodes().size(), is(3));
+            assertThat(dump.getRevisions().get(2).getNodes().size(), is(2)); // node cleared
+            SvnNode firstNode = dump.getRevisions().get(2).getNodes().get(0);
+            SvnNode secondNode = dump.getRevisions().get(2).getNodes().get(1);
 
-        SvnDumpMutator nodeRemove = new NodeRemove(2, "delete", "README2.txt");
-
-        dump.getRevisions().stream().forEach(nodeRemove::consume);
-
-        assertThat(dump.getRevisions().size(), is(3));
-        assertThat(dump.getRevisions().get(0).getNodes().size(), is(0));
-        assertThat(dump.getRevisions().get(1).getNodes().size(), is(3));
-        assertThat(dump.getRevisions().get(2).getNodes().size(), is(2)); // node cleared
-        SvnNode firstNode = dump.getRevisions().get(2).getNodes().get(0);
-        SvnNode secondNode = dump.getRevisions().get(2).getNodes().get(1);
-
-        assertThat(firstNode.get(SvnNodeHeader.PATH), is(not(equalTo("README2.txt"))));
-        assertThat(secondNode.get(SvnNodeHeader.PATH), is(not(equalTo("README2.txt"))));
-
+            assertThat(firstNode.get(SvnNodeHeader.PATH), is(not(equalTo("README2.txt"))));
+            assertThat(secondNode.get(SvnNodeHeader.PATH), is(not(equalTo("README2.txt"))));
+        }
     }
 
     @Test
