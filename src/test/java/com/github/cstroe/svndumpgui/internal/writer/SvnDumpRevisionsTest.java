3235b1feaf2f7903b18333349567caa4c4d2ec63
Cosmin Stroe <cstroe@backstopsolutions.com>
2015-10-02 12:03:07 -0500

    Use the doppelganger.

--- src/test/java/com/github/cstroe/svndumpgui/internal/writer/SvnDumpRevisionsTest.java
+++ src/test/java/com/github/cstroe/svndumpgui/internal/writer/SvnDumpRevisionsTest.java
@@ -3,7 +3,7 @@ package com.github.cstroe.svndumpgui.internal.writer;
 import com.github.cstroe.svndumpgui.api.SvnDump;
 import com.github.cstroe.svndumpgui.api.SvnDumpWriter;
 import com.github.cstroe.svndumpgui.generated.ParseException;
-import com.github.cstroe.svndumpgui.internal.SvnDumpFileParserTest;
+import com.github.cstroe.svndumpgui.internal.utility.SvnDumpFileParserDoppelganger;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
@@ -16,10 +16,10 @@ import static org.junit.Assert.*;
 public class SvnDumpRevisionsTest {
     @Test
     public void print_revisions() throws ParseException, IOException {
-        SvnDump dump = SvnDumpFileParserTest.parse("dumps/add_edit_delete_add.dump");
+        SvnDump dump = SvnDumpFileParserDoppelganger.parse("dumps/add_edit_delete_add.dump");
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         SvnDumpWriter printRevisions = new SvnDumpRevisions();
-        SvnDumpFileParserTest.consume(dump, printRevisions);
+        SvnDumpFileParserDoppelganger.consume(dump, printRevisions);
 
         assertThat(out.toString(), is(equalTo("Finished revision 0.\nFinished revision 1.\nFinished revision 2.\nFinished revision 3.\nFinished revision 4.\n")));
     }
