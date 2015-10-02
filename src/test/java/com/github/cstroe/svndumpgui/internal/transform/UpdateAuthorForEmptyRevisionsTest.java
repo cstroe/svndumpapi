b7da701572cde4f2ca184dd6bd348523c5decefc
Cosmin Stroe <cstroe@backstopsolutions.com>
2015-10-02 11:59:44 -0500

    Use the doppelganger.

--- src/test/java/com/github/cstroe/svndumpgui/internal/transform/UpdateAuthorForEmptyRevisionsTest.java
+++ src/test/java/com/github/cstroe/svndumpgui/internal/transform/UpdateAuthorForEmptyRevisionsTest.java
@@ -4,7 +4,7 @@ import com.github.cstroe.svndumpgui.api.SvnDump;
 import com.github.cstroe.svndumpgui.api.SvnDumpMutator;
 import com.github.cstroe.svndumpgui.api.SvnProperty;
 import com.github.cstroe.svndumpgui.generated.ParseException;
-import com.github.cstroe.svndumpgui.internal.SvnDumpFileParserTest;
+import com.github.cstroe.svndumpgui.internal.utility.SvnDumpFileParserDoppelganger;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
@@ -15,7 +15,7 @@ public class UpdateAuthorForEmptyRevisionsTest {
     @Test
     public void update_empty_revisions() throws ParseException {
         SvnDumpMutator dumpMutator = new ClearRevision(3,4);
-        SvnDump dump = SvnDumpFileParserTest.consume("dumps/add_edit_delete_add.dump", dumpMutator);
+        SvnDump dump = SvnDumpFileParserDoppelganger.consume("dumps/add_edit_delete_add.dump", dumpMutator);
 
         assertThat(dump.getRevisions().size(), is(5));
         assertThat(dump.getRevisions().get(3).get(SvnProperty.AUTHOR), is("cosmin"));
@@ -24,7 +24,7 @@ public class UpdateAuthorForEmptyRevisionsTest {
         assertThat(dump.getRevisions().get(4).getNodes().size(), is(0));
 
         SvnDumpMutator updateAuthor = new UpdateAuthorForEmptyRevisions("blank");
-        SvnDump updatedDump = SvnDumpFileParserTest.consume(dump, updateAuthor);
+        SvnDump updatedDump = SvnDumpFileParserDoppelganger.consume(dump, updateAuthor);
 
         assertThat(updatedDump.getRevisions().size(), is(5));
         assertThat(updatedDump.getRevisions().get(3).get(SvnProperty.AUTHOR), is("blank"));
