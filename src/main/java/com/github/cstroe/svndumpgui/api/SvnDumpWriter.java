a65f64e1df45a9012ea89f4d21fd803f0b879b77
Cosmin Stroe <cstroe@backstopsolutions.com>
2015-10-02 11:36:27 -0500

    Been meaning to get rid of this for a while.

--- src/main/java/com/github/cstroe/svndumpgui/api/SvnDumpWriter.java
+++ src/main/java/com/github/cstroe/svndumpgui/api/SvnDumpWriter.java
@@ -1,25 +1,7 @@
 package com.github.cstroe.svndumpgui.api;
 
-import java.io.IOException;
 import java.io.OutputStream;
-import java.util.Iterator;
 
 public interface SvnDumpWriter extends SvnDumpConsumer {
     void writeTo(OutputStream os);
-
-    default void write(OutputStream os, SvnDump dump) throws IOException {
-        writeTo(os);
-        consume(dump.getPreamble());
-        Iterator<SvnRevision> revisionIter = dump.revisions();
-        while(revisionIter.hasNext()) {
-            SvnRevision revision = revisionIter.next();
-            consume(revision);
-
-            Iterator<SvnNode> nodeIter = revision.nodes();
-            while(nodeIter.hasNext()) {
-                consume(nodeIter.next());
-            }
-        }
-        finish();
-    }
 }
