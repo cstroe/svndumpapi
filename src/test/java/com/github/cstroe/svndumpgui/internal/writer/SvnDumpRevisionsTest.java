c11a0a402df2eaf53051353def497b4ab9f3170e
Cosmin Stroe <cstroe@backstopsolutions.com>
2015-10-02 08:34:33 -0500

    SvnDumpRevisions test should use the new consume method.

--- src/test/java/com/github/cstroe/svndumpgui/internal/writer/SvnDumpRevisionsTest.java
+++ src/test/java/com/github/cstroe/svndumpgui/internal/writer/SvnDumpRevisionsTest.java
@@ -19,7 +19,7 @@ public class SvnDumpRevisionsTest {
         SvnDump dump = SvnDumpFileParserTest.parse("dumps/add_edit_delete_add.dump");
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         SvnDumpWriter printRevisions = new SvnDumpRevisions();
-        printRevisions.write(out, dump);
+        SvnDumpFileParserTest.consume(dump, printRevisions);
 
         assertThat(out.toString(), is(equalTo("Finished revision 0.\nFinished revision 1.\nFinished revision 2.\nFinished revision 3.\nFinished revision 4.\n")));
     }
