99e39b2af6838630afd58273472a07cc2c6c85d2
Cosmin Stroe <cstroe@backstopsolutions.com>
2015-10-02 14:48:31 -0500

    Fix test.

--- src/test/java/com/github/cstroe/svndumpgui/internal/writer/SvnDumpRevisionsTest.java
+++ src/test/java/com/github/cstroe/svndumpgui/internal/writer/SvnDumpRevisionsTest.java
@@ -19,6 +19,7 @@ public class SvnDumpRevisionsTest {
         SvnDump dump = SvnDumpFileParserDoppelganger.parse("dumps/add_edit_delete_add.dump");
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         SvnDumpWriter printRevisions = new SvnDumpRevisions();
+        printRevisions.writeTo(out);
         SvnDumpFileParserDoppelganger.consume(dump, printRevisions);
 
         assertThat(out.toString(), is(equalTo("Finished revision 0.\nFinished revision 1.\nFinished revision 2.\nFinished revision 3.\nFinished revision 4.\n")));
