c197c3a623f1d46a3f0588216e04b88f5b8aae19
Cosmin Stroe <cstroe@backstopsolutions.com>
2015-10-02 14:47:02 -0500

    Assert on the order of consumption.

--- src/test/java/com/github/cstroe/svndumpgui/internal/utility/SvnDumpFileParserDoppelgangerTest.java
+++ src/test/java/com/github/cstroe/svndumpgui/internal/utility/SvnDumpFileParserDoppelgangerTest.java
@@ -1,13 +1,58 @@
 package com.github.cstroe.svndumpgui.internal.utility;
 
+import com.github.cstroe.svndumpgui.api.SvnDumpConsumer;
+import com.github.cstroe.svndumpgui.api.SvnRevision;
+import com.github.cstroe.svndumpgui.internal.SvnDumpImpl;
+import com.github.cstroe.svndumpgui.internal.SvnDumpPreambleImpl;
+import com.github.cstroe.svndumpgui.internal.SvnNodeImpl;
+import com.github.cstroe.svndumpgui.internal.SvnRevisionImpl;
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.jmock.Sequence;
 import org.junit.Test;
-
-import static org.junit.Assert.*;
 
 public class SvnDumpFileParserDoppelgangerTest {
 
     @Test
     public void consume_should_work() {
-        fail(); // TODO: Write this test!
+        final SvnDumpImpl dump = new SvnDumpImpl();
+        SvnDumpPreambleImpl preamble = new SvnDumpPreambleImpl();
+        dump.setPreamble(preamble);
+        SvnRevision r0 = new SvnRevisionImpl(0);
+        dump.addRevision(r0);
+
+        SvnRevision r1 = new SvnRevisionImpl(1);
+        dump.addRevision(r1);
+        SvnNodeImpl n1_1 = new SvnNodeImpl(r1);
+        {
+            r1.addNode(n1_1);
+        }
+
+        SvnRevision r2 = new SvnRevisionImpl(2);
+        dump.addRevision(r2);
+        SvnNodeImpl n2_1 = new SvnNodeImpl(r2);
+        SvnNodeImpl n2_2 = new SvnNodeImpl(r2);
+        {
+            r2.addNode(n2_1);
+            r2.addNode(n2_2);
+        }
+
+        Mockery context = new Mockery();
+
+        SvnDumpConsumer consumer = context.mock(SvnDumpConsumer.class, "c1");
+
+        final Sequence consumerSequence = context.sequence("consumerSequence");
+        context.checking(new Expectations() {{
+            oneOf(consumer).consume(preamble); inSequence(consumerSequence);
+            oneOf(consumer).consume(r0); inSequence(consumerSequence);
+            oneOf(consumer).consume(r1); inSequence(consumerSequence);
+            oneOf(consumer).consume(n1_1); inSequence(consumerSequence);
+            oneOf(consumer).consume(r2); inSequence(consumerSequence);
+            oneOf(consumer).consume(n2_1); inSequence(consumerSequence);
+            oneOf(consumer).consume(n2_2); inSequence(consumerSequence);
+            oneOf(consumer).finish(); inSequence(consumerSequence);
+        }});
+
+        SvnDumpFileParserDoppelganger.consumeWithoutChaining(dump, consumer);
     }
 }
