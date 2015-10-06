ce297a75b9d51a026015cdb281b048e7e06eb094
Cosmin Stroe <cstroe@gmail.com>
2015-10-06 11:25:35 -0500

    SvnDumpSummary fixed to work on with the FileContentChunks change.

--- src/test/java/com/github/cstroe/svndumpgui/internal/utility/SvnDumpSummaryTest.java
+++ src/test/java/com/github/cstroe/svndumpgui/internal/utility/SvnDumpSummaryTest.java
@@ -3,6 +3,7 @@ package com.github.cstroe.svndumpgui.internal.utility;
 import com.github.cstroe.svndumpgui.api.SvnDump;
 import com.github.cstroe.svndumpgui.api.SvnDumpWriter;
 import com.github.cstroe.svndumpgui.generated.ParseException;
+import com.github.cstroe.svndumpgui.generated.SvnDumpFileParser;
 import com.github.cstroe.svndumpgui.internal.SvnDumpFileParserTest;
 import com.github.cstroe.svndumpgui.internal.SvnDumpWriterImplTest;
 import com.github.cstroe.svndumpgui.internal.writer.SvnDumpSummary;
@@ -16,7 +17,7 @@ import java.io.InputStream;
 public class SvnDumpSummaryTest {
 
     @Test
-    public void summarize() throws ParseException, IOException {
+    public void summarize_in_memory() throws ParseException, IOException {
         SvnDump dump = SvnDumpFileParserTest.parse("dumps/svn_multi_file_delete.dump");
 
         SvnDumpWriter summaryWriter = new SvnDumpSummary();
@@ -28,6 +29,22 @@ public class SvnDumpSummaryTest {
                 .getResourceAsStream("summary/svn_multi_file_delete.txt");
 
         SvnDumpWriterImplTest.assertEqualStreams(s, new ByteArrayInputStream(baos.toByteArray()));
+    }
+
+    @Test
+    public void summarize_from_stream() throws ParseException, IOException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        SvnDumpSummary svnDumpSummary = new SvnDumpSummary();
+        svnDumpSummary.writeTo(baos);
+
+        final InputStream inputStream = Thread.currentThread().getContextClassLoader()
+                .getResourceAsStream("dumps/svn_multi_file_delete.dump");
+        SvnDumpFileParser.consume(inputStream, svnDumpSummary);
+
+        InputStream summaryStream = Thread.currentThread().getContextClassLoader()
+                .getResourceAsStream("summary/svn_multi_file_delete.txt");
+
+        SvnDumpWriterImplTest.assertEqualStreams(summaryStream, new ByteArrayInputStream(baos.toByteArray()));
     }
 
 }
