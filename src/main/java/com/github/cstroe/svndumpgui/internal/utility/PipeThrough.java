8318e2e6aaad8d9bac0c86dd83cc0c45fdb3fd16
Cosmin Stroe <cstroe@gmail.com>
2015-10-06 17:07:58 -0500

    A big mess.

--- src/main/java/com/github/cstroe/svndumpgui/internal/utility/PipeThrough.java
+++ src/main/java/com/github/cstroe/svndumpgui/internal/utility/PipeThrough.java
@@ -4,10 +4,14 @@ package com.github.cstroe.svndumpgui.internal.utility;
 import com.github.cstroe.svndumpgui.api.SvnDumpWriter;
 import com.github.cstroe.svndumpgui.generated.ParseException;
 import com.github.cstroe.svndumpgui.generated.SvnDumpFileParser;
-import com.github.cstroe.svndumpgui.internal.SvnDumpWriterImpl;
+import com.github.cstroe.svndumpgui.generated.TokenMgrError;
+import com.github.cstroe.svndumpgui.internal.writer.SvnDumpSummary;
 
+import java.io.FileNotFoundException;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
+
+//import com.github.cstroe.svndumpgui.internal.writer.SvnDumpDebug;
 
 /**
  * Use this from a command line interface to pipe your dump into svndumpgui,
@@ -17,12 +21,21 @@ import java.io.UnsupportedEncodingException;
  */
 public class PipeThrough {
 
-    public static void main(final String[] args) throws ParseException, UnsupportedEncodingException {
-        SvnDumpWriter writer = new SvnDumpWriterImpl();
+    public static void main(final String[] args) throws ParseException, UnsupportedEncodingException, FileNotFoundException {
+        SvnDumpWriter writer = new SvnDumpSummary();
         writer.writeTo(System.out);
+        //new SvnDumpDebug().after(writer);
 
-        SvnDumpFileParser parser = new SvnDumpFileParser(new FastCharStream(new InputStreamReader(System.in, "ISO-8859-1")));
-        parser.Start(writer);
+        //InputStream is = new BufferedInputStream(new FileInputStream("/home/cosmin/Zoo/svndumpgui/svndump"));
+        FastCharStream fastCharStream = new FastCharStream(new InputStreamReader(System.in, "US-ASCII"));
+        SvnDumpFileParser parser = new SvnDumpFileParser(fastCharStream);
+        //parser.setFileContentChunkSize(1024 * 1024 * 32);
+        try {
+            parser.Start(writer);
+        } catch(TokenMgrError err) {
+            System.err.println(err.getMessage());
+            System.err.println("Stream position: " + fastCharStream.getStreamPosition());
+        }
 
         System.out.flush();
     }
