b77596aa60b6c6f4f9f982aab73e724d113dea1d
Cosmin Stroe <cstroe@gmail.com>
2015-10-07 13:46:51 -0500

    Add bash script and a CliConsumer.  This allows us to pipe svnadmin dump directly into our program, and pipe the output directly into svnadmin load.

---
+++ src/main/java/com/github/cstroe/svndumpgui/cli/CliConsumer.java
@@ -0,0 +1,18 @@
+package com.github.cstroe.svndumpgui.cli;
+
+import com.github.cstroe.svndumpgui.api.SvnDumpWriter;
+import com.github.cstroe.svndumpgui.generated.ParseException;
+import com.github.cstroe.svndumpgui.generated.SvnDumpFileParser;
+import com.github.cstroe.svndumpgui.internal.SvnDumpWriterImpl;
+
+import java.io.FileNotFoundException;
+import java.io.UnsupportedEncodingException;
+
+public class CliConsumer {
+    public static void main(final String[] args) throws ParseException, UnsupportedEncodingException, FileNotFoundException {
+        SvnDumpWriter writer = new SvnDumpWriterImpl();
+        writer.writeTo(System.out);
+        SvnDumpFileParser.consume(System.in, writer);
+        System.out.flush();
+    }
+}
