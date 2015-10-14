0ac9a73f37fe05d47f9c25bb3f75d35f10840d06
Cosmin Stroe <cstroe@backstopsolutions.com>
2015-10-14 11:16:54 -0500

    Add the size of the file to the SvnNode toString.

--- src/test/java/com/github/cstroe/svndumpgui/internal/SvnNodeImplTest.java
+++ src/test/java/com/github/cstroe/svndumpgui/internal/SvnNodeImplTest.java
@@ -120,4 +120,22 @@ public class SvnNodeImplTest {
 
         assertThat(node.toString(), is(equalTo("add file new/path.txt eff2191c7e5abb19d79e8bcb2f1b7f38 -- copied from: some/old/path.txt@2 eff2191c7e5abb19d79e8bcb2f1b7f38")));
     }
+
+    @Test
+    public void toString_with_size() {
+        SvnNodeImpl node = new SvnNodeImpl();
+
+        Map<SvnNodeHeader, String> headers = new HashMap<>();
+        headers.put(SvnNodeHeader.MD5, "eff2191c7e5abb19d79e8bcb2f1b7f38");
+        headers.put(SvnNodeHeader.COPY_FROM_PATH, "some/old/path.txt");
+        headers.put(SvnNodeHeader.COPY_FROM_REV, "2");
+        headers.put(SvnNodeHeader.SOURCE_MD5, "eff2191c7e5abb19d79e8bcb2f1b7f38");
+        headers.put(SvnNodeHeader.ACTION, "add");
+        headers.put(SvnNodeHeader.KIND, "file");
+        headers.put(SvnNodeHeader.PATH, "new/path.txt");
+        headers.put(SvnNodeHeader.TEXT_CONTENT_LENGTH, "123456");
+        node.setHeaders(headers);
+
+        assertThat(node.toString(), is(equalTo("add file new/path.txt eff2191c7e5abb19d79e8bcb2f1b7f38 -- copied from: some/old/path.txt@2 eff2191c7e5abb19d79e8bcb2f1b7f38 Size: 123456")));
+    }
 }
--- src/test/java/com/github/cstroe/svndumpgui/internal/utility/SvnDumpSummaryTest.java
+++ src/test/java/com/github/cstroe/svndumpgui/internal/utility/SvnDumpSummaryTest.java
@@ -2,9 +2,16 @@ package com.github.cstroe.svndumpgui.internal.utility;
 
 import com.github.cstroe.svndumpgui.api.SvnDump;
 import com.github.cstroe.svndumpgui.api.SvnDumpWriter;
+import com.github.cstroe.svndumpgui.api.SvnNodeHeader;
+import com.github.cstroe.svndumpgui.api.SvnRevision;
 import com.github.cstroe.svndumpgui.generated.ParseException;
 import com.github.cstroe.svndumpgui.generated.SvnDumpFileParser;
 import com.github.cstroe.svndumpgui.internal.SvnDumpFileParserTest;
+import com.github.cstroe.svndumpgui.internal.SvnDumpImpl;
+import com.github.cstroe.svndumpgui.internal.SvnDumpPreambleImpl;
+import com.github.cstroe.svndumpgui.internal.SvnNodeImpl;
+import com.github.cstroe.svndumpgui.internal.SvnRevisionImpl;
+import com.github.cstroe.svndumpgui.internal.writer.SvnDumpWriterImpl;
 import com.github.cstroe.svndumpgui.internal.writer.SvnDumpWriterImplTest;
 import com.github.cstroe.svndumpgui.internal.writer.SvnDumpSummary;
 import org.junit.Test;
@@ -47,4 +54,39 @@ public class SvnDumpSummaryTest {
         SvnDumpWriterImplTest.assertEqualStreams(summaryStream, new ByteArrayInputStream(baos.toByteArray()));
     }
 
+    @Test
+    public void multiple_empty_revisions() throws IOException, ParseException {
+        SvnDumpImpl dump = new SvnDumpImpl();
+        SvnDumpPreambleImpl preamble = new SvnDumpPreambleImpl("903a69a2-8256-45e6-a9dc-d9a846114b23");
+        dump.setPreamble(preamble);
+        SvnRevision r0 = new SvnRevisionImpl(0);
+        dump.addRevision(r0);
+        SvnRevision r1 = new SvnRevisionImpl(0);
+        dump.addRevision(r1);
+        SvnRevision r2 = new SvnRevisionImpl(0);
+        dump.addRevision(r2);
+        SvnRevision r3 = new SvnRevisionImpl(0);
+        dump.addRevision(r3);
+
+        SvnNodeImpl n3_1 = new SvnNodeImpl(r3);
+        r3.addNode(n3_1);
+        n3_1.getHeaders().put(SvnNodeHeader.ACTION, "add");
+        n3_1.getHeaders().put(SvnNodeHeader.KIND, "dir");
+        n3_1.getHeaders().put(SvnNodeHeader.PATH, "directory1");
+
+        SvnDumpWriterImpl writer = new SvnDumpWriterImpl();
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        writer.writeTo(outputStream);
+        SvnDumpFileParserDoppelganger.consumeWithoutChaining(dump, writer);
+
+        SvnDumpSummary dumpSummary = new SvnDumpSummary();
+        ByteArrayOutputStream summaryStream = new ByteArrayOutputStream();
+        dumpSummary.writeTo(summaryStream);
+        SvnDumpFileParser.consume(new ByteArrayInputStream(outputStream.toByteArray()), dumpSummary);
+
+        InputStream expectedStream = Thread.currentThread().getContextClassLoader()
+                .getResourceAsStream("summary/empty_revisions_at_beginning.txt");
+
+        SvnDumpWriterImplTest.assertEqualStreams(expectedStream, new ByteArrayInputStream(summaryStream.toByteArray()));
+    }
 }
