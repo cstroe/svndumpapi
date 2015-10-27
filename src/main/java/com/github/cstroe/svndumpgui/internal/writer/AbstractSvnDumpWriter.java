889912dd08f39afd1211282d249208eb6b530dfe
Cosmin Stroe <cstroe@backstopsolutions.com>
2015-10-27 09:34:12 -0500

    Remove unused field and accessor.

--- src/main/java/com/github/cstroe/svndumpgui/internal/writer/AbstractSvnDumpWriter.java
+++ src/main/java/com/github/cstroe/svndumpgui/internal/writer/AbstractSvnDumpWriter.java
@@ -7,18 +7,11 @@ import java.io.OutputStream;
 import java.io.PrintStream;
 
 public abstract class AbstractSvnDumpWriter extends AbstractSvnDumpConsumer implements SvnDumpWriter {
-    private OutputStream os;
     private PrintStream ps;
 
     @Override
     public void writeTo(OutputStream os) {
-        this.os = os;
         this.ps = new PrintStream(os);
-    }
-
-    @SuppressWarnings("unused")
-    public OutputStream os() {
-        return os;
     }
 
     public PrintStream ps() {
