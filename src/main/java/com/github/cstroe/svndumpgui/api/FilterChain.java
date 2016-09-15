73fc5ae4c8829417be8dd13aed039b6908730055
Benjamin Scher Purcell <benjapurcell@gmail.com>
2016-09-14 22:18:46 -0500

    On the way towards a new weird api

--- src/main/java/com/github/cstroe/svndumpgui/api/FilterChain.java
+++ src/main/java/com/github/cstroe/svndumpgui/api/FilterChain.java
@@ -1,12 +1,12 @@
 package com.github.cstroe.svndumpgui.api;
 
 public interface FilterChain {
-    void consume(Preamble preamble);
-    void consume(Revision revision);
-    void endRevision(Revision revision);
-    void consume(Node node);
-    void endNode(Node node);
-    void consume(ContentChunk chunk);
-    void endChunks();
-    void finish();
+	void consume(Preamble preamble);
+	void consume(Revision revision);
+	void endRevision(Revision revision);
+	void consume(Node node);
+	void endNode(Node node);
+	void consume(ContentChunk chunk);
+	void endChunks();
+	void finish();
 }
